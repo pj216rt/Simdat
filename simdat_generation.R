387dd2225da8fee6e2d92fdded82d5d3780f4cef
PALMER SWANSON <pjswanson@fsu.edu>
2023-05-18 15:11:44 -0400

    Seperate file for generating simulation data

--- simdat_generation.R
+++ simdat_generation.R
@@ -69,4 +69,6 @@ split.sim2 <- tt_split(datasets = sim2, percent_train = 0.80)
 save(split.sim2, file = "simdata_sim2.RData")
 
 #Condition 3, 15 coefficients of 3, 15 of 0
-sim3 <-
+rep(c(int = 2.0, slope=0.0),7)
+test <- rep(c(0.2,0.4),8)
+length(rep)
